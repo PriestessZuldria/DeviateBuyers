0ac9123903b0e982ecc846cee00e6e4cb685db3c
PriestessZuldria <60475364+PriestessZuldria@users.noreply.github.com>
2020-01-31 05:21:04 -0500

    Update DeviateBuyers.js

--- DeviateBuyers.js
+++ DeviateBuyers.js
@@ -1,6 +1,4 @@
-
-
-
+const functions = {
   save: function() {
       
     let savedPurchases = this;
@@ -8,18 +6,19 @@
     window.localStorage.setItem('savedCharacter', JSON.stringify(savedPurchases));
       
     console.log('The records have been updated and saved. :)');
-},
+  },
 
-load: function() {
-  
-  if (localStorage.getItem('savedPurchases')) {
+  load: function() {
     
-    let savedBuyers = JSON.parse(window.localStorage.getItem('savedPurchases'));
-    
-    console.log('The character was saved successfully.');
-    
-    window.localStorage.clear(); 
-    
-  }
+    if (localStorage.getItem('savedPurchases')) {
+      
+      let savedBuyers = JSON.parse(window.localStorage.getItem('savedPurchases'));
+      
+      console.log('The character was saved successfully.');
+      
+      window.localStorage.clear(); 
+      
+    }
 
-},
+  },
+}
