580f9709e19f6531ff659a04910630f8ac3fc6bb
PriestessZuldria <60475364+PriestessZuldria@users.noreply.github.com>
2020-01-31 05:17:26 -0500

    Add files via upload

---
+++ DeviateBuyers.js
@@ -0,0 +1,25 @@
+
+
+
+  save: function() {
+      
+    let savedPurchases = this;
+      
+    window.localStorage.setItem('savedCharacter', JSON.stringify(savedPurchases));
+      
+    console.log('The records have been updated and saved. :)');
+},
+
+load: function() {
+  
+  if (localStorage.getItem('savedPurchases')) {
+    
+    let savedBuyers = JSON.parse(window.localStorage.getItem('savedPurchases'));
+    
+    console.log('The character was saved successfully.');
+    
+    window.localStorage.clear(); 
+    
+  }
+
+},
